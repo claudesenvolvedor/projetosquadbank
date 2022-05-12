2bf4ffe96673f102b4277f21b1ff06f9e66e8a53
Claudeci Pinheiro <claudesenvolvedor@gmail.com>
2022-05-12 18:29:16 -0300

    Criado o construtor na Class Conta

--- SquadBank/src/conta/Conta.java
+++ SquadBank/src/conta/Conta.java
@@ -6,4 +6,11 @@ public class Conta {
 	private String cpf;
 	private double agencia;
 	private double saldo;
+
+	public Conta(String cliente, String cpf, double agencia, double saldo) {
+		this.cliente = cliente;
+		this.cpf = cpf;
+		this.agencia = agencia;
+		this.saldo = saldo;
+	}
 }
